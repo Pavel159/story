e60625743d8e26d4fa717763d2db09a51e081904
Pavel159 <general9dev@gmail.com>
2023-05-28 22:37:44 +0300

    img path fix

--- src/components/book/data/index.ts
+++ src/components/book/data/index.ts
@@ -1,50 +1,59 @@
+import img1 from '../../../assets/story-img/page1-img.jpg';
+import img2 from '../../../assets/story-img/page2-img.jpg';
+import img3 from '../../../assets/story-img/page3-img.jpg';
+import img4 from '../../../assets/story-img/page4-img.jpg';
+import img5 from '../../../assets/story-img/page5-img.jpg';
+import img6 from '../../../assets/story-img/page6-img.jpg';
+import img7 from '../../../assets/story-img/page7-img.jpg';
+import img8 from '../../../assets/story-img/page8-img.jpg';
+
 const data = [
   {
     id: 1,
     title: 'История создания',
-    imgSrc: 'src/assets/story-img/page1-img.jpg',
+    imgSrc: img1,
     text: 'Эта история берет свое начало в послевоенное время, когда мир пытался справиться с последствиями разрухи.',
     pageNumber: 1,
   },
   {
     id: 2,
-    imgSrc: 'src/assets/story-img/page2-img.jpg',
+    imgSrc: img2,
     text: 'Собрав на последние средства нашего селения экспедицию, мы отправились к берегам Южной Америки,  на последние средства нашего селения экспедицию, мы отправились к берегам Южной Америки, где за легендами...',
     pageNumber: 2,
   },
   {
     id: 3,
-    imgSrc: 'src/assets/story-img/page3-img.jpg',
+    imgSrc: img3,
     text: 'Собрав на последние средства нашего селения экспедицию, мы отправились к берегам Южной Америки, где за легендами...',
     pageNumber: 3,
   },
   {
     id: 4,
-    imgSrc: 'src/assets/story-img/page4-img.jpg',
+    imgSrc: img4,
     text: 'Но нашим мечтам было не суждено сбыться. Судно попало в сильный шторм и было полностью разрушено, а мы вынесены волнами на необитаемый остров.',
     pageNumber: 4,
   },
   {
     id: 5,
-    imgSrc: 'src/assets/story-img/page5-img.jpg',
+    imgSrc: img5,
     text: '7 долгих дней блужданий по острову обессилили нас. Лишь смелость сердец и вера в удачу вела вперед.',
     pageNumber: 5,
   },
   {
     id: 6,
-    imgSrc: 'src/assets/story-img/page6-img.jpg',
+    imgSrc: img6,
     text: 'В тот день, когда отчаяние начало брать верх, впереди замаячил свет костра в непроходимых джунглях. Это казалось чудом. Им оно и было. ',
     pageNumber: 6,
   },
   {
     id: 7,
-    imgSrc: 'src/assets/story-img/page7-img.jpg',
+    imgSrc: img7,
     text: 'В тот день, когда отчаяние начало брать верх, впереди замаячил свет костра в непроходимых джунглях. Это казалось чудом. Им оно и было.',
     pageNumber: 7,
   },
   {
     id: 8,
-    imgSrc: 'src/assets/story-img/page8-img.jpg',
+    imgSrc: img8,
     text: 'У костра сидела девушка, красота которой затмевала лунный свет и заставляла сердце биться чаще.',
     pageNumber: 8,
   },
