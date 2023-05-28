e8e300c4d743d9ba632bc0da86342e7feb4861d3
Pavel159 <general9dev@gmail.com>
2023-05-28 22:17:43 +0300

    small syntax fix

--- src/components/book/book-cover/BookCover.tsx
+++ src/components/book/book-cover/BookCover.tsx
@@ -3,7 +3,7 @@ import cl from './BookCover.module.scss';
 
 export type RefType = HTMLDivElement;
 
-const BookCover = React.forwardRef<RefType>((props, ref) => {
+const BookCover = React.forwardRef<RefType>((_props, ref) => {
   return <div className={cl.bookCover} ref={ref} data-density='hard'></div>;
 });
 
